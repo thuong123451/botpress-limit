268ba051f92591367faedf936b3f78217ff34af2
thuong123451 <thuong1610991@gmail.com>
2025-04-25 13:25:14 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,11 @@
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore as _getFirestore } from 'firebase-admin/firestore';
 
+let app;
+export function getFirestore(env) {
+  if (!app) {
+    const serviceAccount = JSON.parse(env.FIREBASE_SERVICE_ACCOUNT);
+    app = initializeApp({ credential: cert(serviceAccount) });
+  }
+  return _getFirestore(app);
+}
